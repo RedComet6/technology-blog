4947dc5424fe509a03ada73d36fbac91a1ac6d8a
Aidan Chamberlain <asunshine99@gmail.com>
2022-04-03 20:00:06 -0500

    CSS updates, fixed nav bar for update page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,6 +49,7 @@ router.get('/update/:id', async (req, res) => {
 
     res.render('update', {
       ...post,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
